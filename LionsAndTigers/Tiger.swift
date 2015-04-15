2138299fe55cbd8fe9e50c50ca467f894948c0f6
jxxnb <jbruno404@gmail.com>
2015-04-15 15:27:00 -0400

    First Rev - For section on OOP

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by John Bruno on 2015-04-15.
+//  Copyright (c) 2015 John Bruno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+}
